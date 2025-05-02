b222d78042ab88b909b94b8d74d2a3744d6eed30
Hrushi-bagade <88029794+Hrushi-bagade@users.noreply.github.com>
2025-05-02 08:14:38 +0000

    Main Layout FIx

--- AGR_ChatAI/src/layouts/MainLayout.jsx
+++ AGR_ChatAI/src/layouts/MainLayout.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
-// import '../assets/css/_layout.css';
-import '../App.css'
-
 const MainLayout = () => (
-  <div className="app-layout">
-    <Sidebar/>
-    {/* <header className="layout-header">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/dashboard">Dashboard</Link>
-      </nav>
-    </header> */}
-    <main className="layout-content">
+  <>
+<div className="app-layout">
+    <Sidebar />
+    <main className="app-main">
       <Outlet />
     </main>
-    {/* <footer className="layout-footer">
-      © 2025 MyApp
-    </footer> */}
   </div>
+  </>
 );
 
 export default MainLayout;
